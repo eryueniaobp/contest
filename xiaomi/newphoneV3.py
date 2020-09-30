5b59c43cbd7a8acc5882f6c78b43f58b984b6ada
peng <baipeng8608@gmail.com>
2020-09-30 09:52:43 +0800

    use FM layer

--- xiaomi/newphoneV3.py
+++ xiaomi/newphoneV3.py
@@ -7,7 +7,56 @@ import tensorflow as tf
 import numpy as np
 import argparse
 
-from FMLayer import FM
+
+
+import keras.backend as K
+class FM(tf.keras.layers.Layer):
+    """Factorization Machine models pairwise (order-2) feature interactions
+     without linear term and bias.
+
+      Input shape
+        - 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+
+      Output shape
+        - 2D tensor with shape: ``(batch_size, 1)``.
+
+      References
+        - [Factorization Machines](https://www.csie.ntu.edu.tw/~b97053/paper/Rendle2010FM.pdf)
+    """
+
+    def __init__(self, **kwargs):
+
+        super(FM, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if len(input_shape) != 3:
+            raise ValueError("Unexpected inputs dimensions % d,\
+                             expect to be 3 dimensions" % (len(input_shape)))
+
+        super(FM, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, inputs, **kwargs):
+
+        if K.ndim(inputs) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions"
+                % (K.ndim(inputs)))
+
+        concated_embeds_value = inputs
+        square_of_sum = tf.square(tf.reduce_sum(
+            concated_embeds_value, axis=1, keepdims=True))
+        sum_of_square = tf.reduce_sum(
+            concated_embeds_value * concated_embeds_value, axis=1, keepdims=True)
+        cross_term = square_of_sum - sum_of_square
+        cross_term = 0.5 * tf.reduce_sum(cross_term, axis=2, keepdims=False)
+
+        return cross_term
+
+
+    def compute_output_shape(self, input_shape):
+        return (None, 1)
+
+
 class Constant:
     prefix = "/home/baiyun/PycharmProjects/miwin/data_store/feature"
     nagtive_count = 392972.0
